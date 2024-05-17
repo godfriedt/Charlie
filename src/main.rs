2243aa2bc26030e912a6ce2abbbf9777bb99b1f2
godfriedt <60560876+godfriedt@users.noreply.github.com>
2024-05-17 13:52:59 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(str_split_remainder)]
+
 use std::env;
 
 mod listener;
@@ -5,14 +7,16 @@ mod connect;
 
 fn main() {
 
+    // get args
     let args: Vec<String> = env::args().collect();
 
+    // driver code
     let socket_function = match args[1].as_str() {
         "listen" => listener::socket_listen,
         "connect" => connect::socket_connect,
-        _ => panic!("Please choose either -c or -s to start the client or server respectively.")
+        _ => panic!("Please choose either listen or connect to start the listener or connector respectively.")
     };
 
+    // call function
     socket_function(&args[2], &args[3]);
-
 }
